af00c3327bed2d8a1ce10e2ce5ac7f4a8e7679ae
Ubaid Mehmood <ubaidwarraich001@gmail.com>
2021-07-13 01:05:11 +0500

    f error

--- src/main/java/com/javainuse/db/eventRepository.java
+++ src/main/java/com/javainuse/db/eventRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.javainuse.model.Event;
 
 public interface eventRepository extends JpaRepository<Event, Long> {
-	Optional<Event> findTopByName(String name);
+	Optional<Event> findTopByTitle(String title);
 }
