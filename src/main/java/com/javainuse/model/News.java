084d8fb5435c2a05da50f0890ee366db327897ca
Ubaid Mehmood <ubaidwarraich001@gmail.com>
2021-07-13 23:33:21 +0500

    changing yo know

--- src/main/java/com/javainuse/model/News.java
+++ src/main/java/com/javainuse/model/News.java
@@ -1,11 +1,9 @@
 package com.javainuse.model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
-
+@Entity
+@Table(name = "news")
 public class News {
     public News() {
         super();
