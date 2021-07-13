5748f64d6a7e358f06530ed7ff24585afdbb0db9
Ubaid Mehmood <ubaidwarraich001@gmail.com>
2021-07-13 15:27:11 +0500

    fixing

--- src/main/java/com/javainuse/controller/EventController.java
+++ src/main/java/com/javainuse/controller/EventController.java
@@ -2,6 +2,7 @@ package com.javainuse.controller;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
@@ -18,7 +19,7 @@ import com.javainuse.db.eventRepository;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:4201", "http://localhost:4200"})
-@RequestMapping(path = "image")
+@RequestMapping(path = "event")
 public class EventController {
 
     @Autowired
@@ -28,8 +29,37 @@ public class EventController {
     imageRepository imageRepository;
 
     @PostMapping()
-    public Event createEvent(@RequestBody Event event){
+    public Event createEvent(@RequestBody Event event) {
         return eventRepository.save(event);
+    }
+
+    @GetMapping("/all")
+    public List<Event> getAll() {
+        return eventRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Event> getById(@PathVariable("id") Long id) {
+        return eventRepository.findById(id);
+    }
+    
+    @PutMapping("/{id}")
+    public Event updateEvent(@PathVariable("id") Long id,@RequestBody Event event){
+        Optional<Event> event1=eventRepository.findById(id);
+        return event1.map(event2 -> {
+            event2.setTitle(event.getTitle());
+            event2.setDescription(event.getDescription());
+            event2.setDate(event.getDate());
+            event2.setVenue(event.getVenue());
+            event2.setImageId(event.getImageId());
+           return eventRepository.save(event2);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public Event DeleteEvent(@PathVariable("id") Long id){
+         eventRepository.deleteById(id);
+         return null;
     }
 
     @PostMapping("/upload")
